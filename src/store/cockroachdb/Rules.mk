18882c863eb868f1579608e0f27bd941a2518eef
bcyl2 <bcyl2@localhost>
2023-03-09 20:16:59 -0700

    Exclude client for now

--- src/store/cockroachdb/Rules.mk
+++ src/store/cockroachdb/Rules.mk
@@ -5,7 +5,8 @@ SRCS += $(addprefix $(d), server.cc )
 # TaoPQ static libraries
 LIB-taopq :=
 
-LIB-cockroachdb-store := $(o)common.o $(o)slots.o $(o)replica.o $(o)server.o \
-	$(o)server-proto.o $(o)app.o $(o)shardclient.o \
-	$(o)client.o $(LIB-crypto) $(LIB-configuration) $(LIB-store-common) \
-	$(LIB-transport) 
+LIB-cockroachdb-store := $(o)server.o \
+	$(LIB-crypto) $(LIB-configuration) $(LIB-store-common) \
+	$(LIB-transport) \
+	# $(o)client.o 
+
