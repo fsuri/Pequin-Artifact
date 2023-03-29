8586871934bf6cae76fb535edc5ce0e811dbdb5a
Benton Li <bli528@bloomberg.net>
2023-03-29 09:58:55 -0600

    Add crdb testers

---
+++ src/crdb-client-tester.sh
@@ -0,0 +1,15 @@
+store/benchmark/async/benchmark --config_path ./store/testing/toy.config \
+ --num_groups 1 \
+ --num_shards 1 \
+ --protocol_mode crdb \
+ --num_keys 1 \
+ --benchmark rw \
+ --num_ops_txn 2 \
+ --exp_duration 10 \
+ --client_id 0 \
+ --warmup_secs 0 \
+ --cooldown_secs 0 \
+ --key_selector zipf \
+ --zipf_coefficient 0.0 \
+ --stats_file "stats-0.json" \
+ --indicus_key_path keys &> client-0.out &
