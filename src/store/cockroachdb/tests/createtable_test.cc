c811c6850d3a2e2ed00137d1c6da6b29fe7f0e23
Benton Li <bli528@bloomberg.net>
2023-04-10 00:35:03 -0600

    Update example

--- src/store/cockroachdb/tests/createtable_test.cc
+++ src/store/cockroachdb/tests/createtable_test.cc
@@ -1,14 +1,14 @@
-#include <stdlib.h>
-using namespace std;
-const std::string table_name = "users";
+const std::string table_name = "students";
 const std::vector<std::pair<std::string, std::string>> column_data_types = {
-    std::make_pair("id", "UUID"),
-    std::make_pair("city", "STRING"),
-    std::make_pair("name", "STRING"),
-    std::make_pair("address", "STRING"),
-    std::make_pair("credit_card", "STRING"),
-    std::make_pair("dl", "STRING")};
-const std::vector<uint32_t> pr imary_key_col_idx = {0};
-if (FLAGS_replica_idx == 2)
+    std::make_pair("rollNumber", "INT"), std::make_pair("name", "VARCHAR(30)"),
+    std::make_pair("class", "VARCHAR(30)"),
+    std::make_pair("section", "VARCHAR(30)"),
+    std::make_pair("mobile", "VARCHAR(30)")};
+const std::vector<uint32_t> primary_key_col_idx = {0, 1};
+std::vector<std::string> values = {"1", "BEN", "FOURTH", "B", "4204206969"};
+if (FLAGS_replica_idx == 2) {
   server->CreateTable(table_name, column_data_types, primary_key_col_idx);
+  server->LoadTableRow(table_name, column_data_types, values,
+                       primary_key_col_idx);
+}
 break;
