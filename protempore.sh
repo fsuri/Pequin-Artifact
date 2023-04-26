493d6e001f13edbc8ec541c1c8e8a0d4d69f0f5f
Benton Li <bli528@bloomberg.net>
2023-04-26 00:28:11 -0600

    update protempore

--- protempore.sh
+++ protempore.sh
@@ -1,1 +1,2 @@
+rsync -v -r -e ssh test-1k.csv bcyl2@us-east-1-0.pequin.pequin-pg0.utah.cloudlab.us:/users/bcyl2/indicus/bin/
 python experiment-scripts/run_multiple_experiments.py testing/CRDB-config-RW-control.json 
