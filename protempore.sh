e8a78d825633d5f29d223fa3161b62d8a55a5fbe
Benton Li <bli528@bloomberg.net>
2023-05-08 13:02:02 -0600

    Add more expmt scripts

---
+++ protempore.sh
@@ -0,0 +1,5 @@
+bash testing/upload_test_data-control.sh 
+python experiment-scripts/run_multiple_experiments.py testing/CRDB-config-RW-control.json 
+
+python experiment-scripts/run_multiple_experiments.py testing/CRDB-TPCC-control.json
+
