70748e08d10b6a6adff5559186d754f4c824f226
ojmatte <ojmatte@gmail.com>
2023-06-12 12:07:40 -0500

    Now passing serialized message

--- src/store/hotstuffpgstore/server.cc
+++ src/store/hotstuffpgstore/server.cc
@@ -413,6 +413,9 @@ std::vector<::google::protobuf::Message*> Server::HandleTransaction(const proto:
       }
     }
     reply->set_status(REPLY_OK);
+    std::string* res_string;
+    res_builder->get_result()->SerializeToString(res_string);
+    reply->set_sql_res(*res_string); //&
   } catch(tao::pq::sql_error e) {
     reply->set_status(REPLY_FAIL);
   }
