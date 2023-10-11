4894aaf00d574d9a68a8d39cfda05d976568cc37
root <root@localhost>
2023-10-11 09:10:19 -0600

    fixed peloton dependencies install

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -185,10 +185,10 @@ cd ..
 echo "--libpg_query"
 echo ""
 git clone https://github.com/cmu-db/peloton.git
-cd peloton/third_party/libpgquery
+cd peloton/third_party/libpg_query
 sudo make
 cd ..
-sudo cp -r libpgquery /usr/local/include
+sudo cp -r libpg_query /usr/local/include
 sudo cp libpg_query/libpg_query.a /usr/local/lib
 
 echo "--libcuckoo"
