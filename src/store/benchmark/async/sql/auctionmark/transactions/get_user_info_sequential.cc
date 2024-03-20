74c948c8217a16f54440229ac38253bc0a5f16fb
fsuri <fs435@cornell.edu>
2024-03-20 11:04:22 -0400

    auction use parallel get_user_info again

--- src/store/benchmark/async/sql/auctionmark/transactions/get_user_info_sequential.cc
+++ src/store/benchmark/async/sql/auctionmark/transactions/get_user_info_sequential.cc
@@ -71,6 +71,9 @@ transaction_status_t GetUserInfo::Execute(SyncClient &client) {
   Debug("Get Buyer Items: %lu", get_buyer_items);
   Debug("Get Feedback: %lu", get_feedback);
 
+  std::vector<ItemCommentResponse> item_comment_responses;
+  std::vector<ItemRecord> item_records;
+
 
   client.Begin(timeout);
 
@@ -79,20 +82,21 @@ transaction_status_t GetUserInfo::Execute(SyncClient &client) {
   //TODO: parallelize tx that can be concurrent.
 
   //getUser 
-   statement = fmt::format("SELECT u_id, u_rating, u_created, u_balance, u_sattr0, u_sattr1, u_sattr2, u_sattr3, u_sattr4, r_name "
+  statement = fmt::format("SELECT u_id, u_rating, u_created, u_balance, u_sattr0, u_sattr1, u_sattr2, u_sattr3, u_sattr4, r_name "
                           "FROM {}, {} "
                           "WHERE u_id = '{}' AND u_r_id = r_id "
                           "AND r_id = r_id", //ADDED REFLEXIVE ARG FOR PELOTON PARSING. TODO: AUTOMATE THIS IN SQL_INTERPRETER 
                           TABLE_USERACCT, TABLE_REGION, user_id);
-  client.Query(statement, timeout);
+  client.Query(statement, queryResult, timeout);
 
   if(get_feedback){
      //getUserFeedback
       std::cerr << "getUserFeedback" << std::endl;
-    statement = fmt::format("SELECT u_id, u_rating, u_sattr0, u_sattr1, uf_rating, uf_date, uf_sattr0 "
-                          "FROM {}, {} WHERE u_id = '{}' AND uf_u_id = '{}' AND uf_u_id = u_id ORDER BY uf_date DESC LIMIT 25", TABLE_USERACCT, TABLE_USERACCT_FEEDBACK, user_id, user_id); 
+    statement = fmt::format("SELECT u_id, u_rating, u_sattr0, u_sattr1, uf_rating, uf_date, uf_sattr0  FROM {}, {} "
+                            "WHERE u_id = '{}' AND uf_u_id = '{}' AND uf_u_id = u_id "
+                            "ORDER BY uf_date DESC LIMIT 25", TABLE_USERACCT, TABLE_USERACCT_FEEDBACK, user_id, user_id); 
                                                         //redundant input for better Peloton plan...
-    client.Query(statement, timeout);
+    client.Query(statement, queryResult, timeout);
   }
  
   if(get_comments){
@@ -104,56 +108,8 @@ transaction_status_t GetUserInfo::Execute(SyncClient &client) {
                             "AND ic_i_id = ic_i_id AND ic_u_id = ic_u_id " //ADDED REFLEXIVE ARG FOR PELOTON PARSING. TODO: AUTOMATE THIS IN SQL_INTERPRETER 
                             "ORDER BY ic_created DESC LIMIT 25", 
                             ITEM_COLUMNS_STR, TABLE_ITEM, TABLE_ITEM_COMMENT, user_id, ItemStatus::OPEN);
-    client.Query(statement, timeout);
-  }
-
-  if(get_seller_items){
-    //getSellerItems
-    std::cerr << "getSellerItems" << std::endl;
-    statement = fmt::format("SELECT {} FROM {} WHERE i_u_id = '{}' ORDER BY i_end_date DESC LIMIT 25", ITEM_COLUMNS_STR, TABLE_ITEM, user_id);
-    client.Query(statement, timeout); 
-  }
-              
-  if(get_buyer_items){ 
-     //getBuyerItems
-    std::cerr << "getBuyerItems" << std::endl;
-   statement = fmt::format("SELECT {} FROM {}, {} "
-                            "WHERE ui_u_id = '{}' AND ui_i_id = i_id AND ui_i_u_id = i_u_id " 
-                            "AND i_id = i_id AND i_u_id = i_u_id " //ADDED REFLEXIVE ARG FOR PELOTON PARSING. TODO: AUTOMATE THIS IN SQL_INTERPRETER 
-                            "ORDER BY i_end_date DESC LIMIT 25", 
-                             ITEM_COLUMNS_STR, TABLE_USERACCT_ITEM, TABLE_ITEM, user_id); //user_id == bidder
-    client.Query(statement, timeout);
-  }
-
-  if(get_watched_items){
-    //getWatchedItems
-     std::cerr << "geWatchedItems" << std::endl;
-    statement = fmt::format("SELECT {}, uw_u_id, uw_created FROM {}, {} "
-                            "WHERE uw_u_id = '{}' AND uw_i_id = i_id AND uw_i_u_id = i_u_id " 
-                            "AND i_id = i_id AND i_u_id = i_u_id " //ADDED REFLEXIVE ARG FOR PELOTON PARSING. TODO: AUTOMATE THIS IN SQL_INTERPRETER 
-                            "ORDER BY i_end_date DESC LIMIT 25", 
-                            ITEM_COLUMNS_STR, TABLE_USERACCT_WATCH, TABLE_ITEM, user_id); //user_id = buyer; i_u_id = seller
-    client.Query(statement, timeout); 
-  }
-  
-
-  client.Wait(results);
-
-  Debug("COMMIT");
-  auto tx_result = client.Commit(timeout);
-  if(tx_result != transaction_status_t::COMMITTED) return tx_result;
-
-  //////////////// UPDATE PROFILE /////////////////////
-
-  //Deserialize
-  int offset = 1;
-
-  if(get_feedback){
-
-    offset++;
-  }
-  if(get_comments){
-    queryResult = std::move(results[offset]);
+    client.Query(statement, queryResult, timeout);
+   
     for(int i = 0; i < queryResult->size(); ++i){
       std::string itemId;
       std::string sellerId;
@@ -162,50 +118,76 @@ transaction_status_t GetUserInfo::Execute(SyncClient &client) {
       deserialize(sellerId, queryResult, i, 1);
       deserialize(commentId, queryResult, i, 7);
 
-      ItemCommentResponse cr(commentId, itemId, sellerId);
-      profile.add_pending_item_comment_response(cr);
+      item_comment_responses.push_back(ItemCommentResponse(commentId, itemId, sellerId));
     }
-    offset++;
+   //Panic("stop after comment");
   }
+
   if(get_seller_items){
-     queryResult = std::move(results[offset]);
+    //getSellerItems
+    std::cerr << "getSellerItems" << std::endl;
+    statement = fmt::format("SELECT {} FROM {} WHERE i_u_id = '{}' ORDER BY i_end_date DESC LIMIT 25", ITEM_COLUMNS_STR, TABLE_ITEM, user_id);
+    client.Query(statement, queryResult, timeout);
     for(int i=0; i < queryResult->size(); ++i){
        ItemRow ir;
       deserialize(ir, queryResult, i);
-       std::cerr << "next seller row" << std::endl;  
       
-      ItemRecord item_rec(ir.itemId, ir.sellerId, ir.i_name, ir.currentPrice, ir.numBids, ir.endDate, ir.itemStatus);
-      ItemId itemId = profile.processItemRecord(item_rec);
+      item_records.push_back(ItemRecord(ir.itemId, ir.sellerId, ir.i_name, ir.currentPrice, ir.numBids, ir.endDate, ir.itemStatus));
     }
-    offset++;
+   
   }
+              
   if(get_buyer_items){
-    queryResult = std::move(results[offset]);
-    for(int i=0; i < queryResult->size(); ++i){
-      ItemRow ir;
-    deserialize(ir, queryResult, i);
-    
-    ItemRecord item_rec(ir.itemId, ir.sellerId, ir.i_name, ir.currentPrice, ir.numBids, ir.endDate, ir.itemStatus);
-    ItemId itemId = profile.processItemRecord(item_rec);
-    }
-    offset++;
-  }
-  if(get_watched_items){
-     queryResult = std::move(results[offset]);
+     //getBuyerItems
+    std::cerr << "getBuyerItems" << std::endl;
+    statement = fmt::format("SELECT {} FROM {}, {} "
+                            "WHERE ui_u_id = '{}' AND ui_i_id = i_id AND ui_i_u_id = i_u_id " 
+                            "AND i_id = i_id AND i_u_id = i_u_id " //ADDED REFLEXIVE ARG FOR PELOTON PARSING. TODO: AUTOMATE THIS IN SQL_INTERPRETER 
+                            "ORDER BY i_end_date DESC LIMIT 25", 
+                             ITEM_COLUMNS_STR, TABLE_USERACCT_ITEM, TABLE_ITEM, user_id); //user_id == bidder
+    client.Query(statement, queryResult, timeout);
   
      for(int i=0; i < queryResult->size(); ++i){
        ItemRow ir;
       deserialize(ir, queryResult, i);
       
-      ItemRecord item_rec(ir.itemId, ir.sellerId, ir.i_name, ir.currentPrice, ir.numBids, ir.endDate, ir.itemStatus);
-      ItemId itemId = profile.processItemRecord(item_rec);
+      item_records.push_back(ItemRecord(ir.itemId, ir.sellerId, ir.i_name, ir.currentPrice, ir.numBids, ir.endDate, ir.itemStatus));
     }
-    offset++;
+
+  }
+
+  if(get_watched_items){
+    //getWatchedItems
+     std::cerr << "getWatchedItems" << std::endl;
+    statement = fmt::format("SELECT {}, uw_u_id, uw_created FROM {}, {} "
+                            "WHERE uw_u_id = '{}' AND uw_i_id = i_id AND uw_i_u_id = i_u_id " 
+                            "AND i_id = i_id AND i_u_id = i_u_id " //ADDED REFLEXIVE ARG FOR PELOTON PARSING. TODO: AUTOMATE THIS IN SQL_INTERPRETER 
+                            "ORDER BY i_end_date DESC LIMIT 25", 
+                            ITEM_COLUMNS_STR, TABLE_USERACCT_WATCH, TABLE_ITEM, user_id); //user_id = buyer; i_u_id = seller
+    client.Query(statement, queryResult, timeout);
+     for(int i=0; i < queryResult->size(); ++i){
+       ItemRow ir;
+      deserialize(ir, queryResult, i);
+      
+      item_records.push_back(ItemRecord(ir.itemId, ir.sellerId, ir.i_name, ir.currentPrice, ir.numBids, ir.endDate, ir.itemStatus));
+    }
+  }
+
+   
+  Debug("COMMIT");
+  auto tx_result = client.Commit(timeout);
+  if(tx_result != transaction_status_t::COMMITTED) return tx_result;
+   
+   //////////////// UPDATE PROFILE /////////////////////
+  for(auto &item_cr: item_comment_responses){
+     profile.add_pending_item_comment_response(item_cr);
+  }
+  for(auto &item_rec: item_records){
+    ItemId itemId = profile.processItemRecord(item_rec);
   }
 
 
-
-  return tx_result;
+ return tx_result;
 }
 
 } // namespace auctionmark
