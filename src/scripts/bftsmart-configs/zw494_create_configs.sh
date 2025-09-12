c58e4ffe0a27228c2723249c5da660971726914d
fsuri <fs435@cornell.edu>
2025-09-11 21:28:11 -0400

    nit update to bftsmart config script

--- src/scripts/bftsmart-configs/zw494_create_configs.sh
+++ src/scripts/bftsmart-configs/zw494_create_configs.sh
@@ -2,7 +2,7 @@
 rm -r $1/src/store/bftsmartstore/library/java-config-*
 rm -r $1/src/store/bftsmartstore/library/remote
 mkdir $1/src/store/bftsmartstore/library/remote
-cat $1/src/scripts/server-hosts | while read machine
+cat $1/src/scripts/bftsmart-configs/server-hosts | while read machine
 do
     if [[ $machine =~ [:digit:] ]]; then 
     echo "generating config file for machine ${machine}";
@@ -10,7 +10,7 @@ do
     cp -r $1/src/store/bftsmartstore/library/java-config $1/src/store/bftsmartstore/library/remote/java-config-${machine}/java-config;
     fi
 done
-cat $1/src/scripts/client-hosts | while read machine
+cat $1/src/scripts/bftsmart-configs/client-hosts | while read machine
 do
     echo "generating config file for machine ${machine}"
     mkdir $1/src/store/bftsmartstore/library/remote/java-config-${machine}
